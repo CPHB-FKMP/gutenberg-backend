d78fbf5ebed1949350685702c270d67812e8ef50
philliphb <philliphb@gmail.com>
2018-05-07 12:06:51 +0200

    Added integrations test Neo4j and Postgres #7

---
+++ src/main/java/com/fkmp/gutenberg/backend/model/Book.java
@@ -0,0 +1,13 @@
+package com.fkmp.gutenberg.backend.model;
+
+public class Book {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ src/test/java/com/fkmp/gutenberg/backend/SystemTest.java
@@ -0,0 +1,14 @@
+package com.fkmp.gutenberg.backend;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles(profiles = "frontend", inheritProfiles = false)
+public class SystemTest {
+
+
+}
