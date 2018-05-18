8355335034e1b45802e82e66aa9dc689e0d5b963
Mikkel Ziemer <mzhjensen@gmail.com>
2018-05-18 12:32:23 +0200

    Updated script to reflect newly accepted changes

--- src/main/resources/PostgreSQL.sql
+++ src/main/resources/PostgreSQL.sql
@@ -1,30 +1,34 @@
-DROP TABLE IF EXISTS books_locations, authors_books, author, location, book;
+DROP TABLE IF EXISTS books_cities, authors_books, authors, cities, books;
 
-CREATE TABLE IF NOT EXISTS author(
-    id bigint PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS authors (
+    author_id bigint PRIMARY KEY,
     name varchar
 );
 
-CREATE TABLE IF NOT EXISTS book(
-    id varchar PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS books (
+    book_id varchar PRIMARY KEY,
     title varchar
 );
 
-CREATE TABLE IF NOT EXISTS location(
-    latitude double precision,
-    longitude double precision,
-    city varchar,
-    PRIMARY KEY (latitude, longitude)
+CREATE TABLE IF NOT EXISTS cities (
+    name varchar,
+    location point PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS authors_books(
-    authorid bigint references author(id),
-    bookid varchar references book(id)
+    book_id varchar references book(id),
+    author_id bigint references author(id)
 );
 
 CREATE TABLE IF NOT EXISTS books_locations(
-    bookid varchar references book(id),
-    latitude double precision,
-    longitude double precision,
-    FOREIGN KEY (latitude, longitude) references location(latitude, longitude) 
+    book_id varchar references book(id),
+    location point references cities(location)
 );
+
+CREATE INDEX city_name_index ON cities USING btree(name);
+
+CREATE INDEX book_title_index ON books USING btree(title);
+
+CREATE INDEX author_name_index ON authors USING btree(name);
+
+CREATE INDEX city_location_index ON cities USING gist(location);
