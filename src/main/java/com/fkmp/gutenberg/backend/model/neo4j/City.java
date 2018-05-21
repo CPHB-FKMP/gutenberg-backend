9b49388d7e4262582fb1faebfd7b96999e45dda7
Mikkel Ziemer <mzhjensen@gmail.com>
2018-05-21 18:40:38 +0200

    Fixed wrong imports

--- src/main/java/com/fkmp/gutenberg/backend/model/neo4j/City.java
+++ src/main/java/com/fkmp/gutenberg/backend/model/neo4j/City.java
@@ -1,12 +1,11 @@
 package com.fkmp.gutenberg.backend.model.neo4j;
 
-import com.fkmp.gutenberg.backend.model.AbstractCity;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.springframework.data.geo.Point;
 
 @NodeEntity
-public class City extends AbstractCity {
+public class City {
 
     private String name;
 
--- src/main/java/com/fkmp/gutenberg/backend/model/postgres/City.java
+++ src/main/java/com/fkmp/gutenberg/backend/model/postgres/City.java
@@ -1,13 +1,12 @@
 package com.fkmp.gutenberg.backend.model.postgres;
 
-import com.fkmp.gutenberg.backend.model.AbstractCity;
 import org.springframework.data.geo.Point;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class City extends AbstractCity {
+public class City {
 
     private String name;
 
