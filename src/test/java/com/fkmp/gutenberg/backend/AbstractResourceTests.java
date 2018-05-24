fd96068402c1c5580686537f0fdea0818fe77c2b
Mikkel Ziemer <mzhjensen@gmail.com>
2018-05-24 10:48:16 +0200

    Fixed a lot of tests

--- src/test/java/com/fkmp/gutenberg/backend/AbstractResourceTests.java
+++ src/test/java/com/fkmp/gutenberg/backend/AbstractResourceTests.java
@@ -1,6 +1,7 @@
 package com.fkmp.gutenberg.backend;
 
 import com.fkmp.gutenberg.backend.api.model.BookDto;
+import com.fkmp.gutenberg.backend.api.model.City;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.junit.Assert;
@@ -16,12 +17,15 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 
 @RunWith(SpringRunner.class)
@@ -55,13 +59,82 @@ public abstract class AbstractResourceTests {
         return t.request();
     }
 
-    protected void testArray(ArrayList<BookDto> expected, ArrayList<BookDto> actual) {
+    protected void getBooksOnCityTest(String path) {
+        // Expected book list
+        ArrayList<BookDto> expectedBooks = new ArrayList<>();
+        BookDto bookDto1 = new BookDto();
+        bookDto1.setTitle("My Lady of the Chinese Courtyard");
+        bookDto1.setId("19665");
+
+        BookDto bookDto2 = new BookDto();
+        bookDto2.setTitle("The Return of Peter Grimm Novelised From the Play");
+        bookDto2.setId("24359");
+
+        BookDto bookDto3 = new BookDto();
+        bookDto3.setTitle("Morals and the Evolution of Man");
+        bookDto3.setId("37998");
+
+        BookDto bookDto4 = new BookDto();
+        bookDto4.setTitle("History of the Reformation in the Sixteenth Century, Vol 2");
+        bookDto4.setId("41470");
+
+        expectedBooks.add(bookDto1);
+        expectedBooks.add(bookDto2);
+        expectedBooks.add(bookDto3);
+        expectedBooks.add(bookDto4);
+
+        // Create Parameter for the query and sends a request
+        MultivaluedMap<String, String> params = new MultivaluedHashMap<>();
+        params.putSingle("city", "London");
+        Response response = getRequest(path, params).get();
+
+        Assert.assertEquals(200, response.getStatus());
+        // Collect the result from the response
+        ArrayList<BookDto> result = response.readEntity(BookList.class);
+
+        Assert.assertTrue(!result.get(0).getAuthors().isEmpty());
+
+        testArray(expectedBooks, result);
+    }
+
+    protected void getBooksOnTitleTest(String path) {
+        // Expected book list
+        ArrayList<BookDto> expectedBooks = new ArrayList<>();
+        BookDto bookDto = new BookDto();
+        bookDto.setId("21271");
+        bookDto.setTitle("Unknown title");
+
+        ArrayList<City> cities = new ArrayList<>();
+        City city1 = new City();
+        city1.setName("Young");
+        city1.setLatitude("-32.698440");
+        city1.setLongitude("-57.626930");
+
+        City city2 = new City();
+        city2.setName("Roses");
+        city2.setLatitude("42.261990");
+        city2.setLongitude("3.176890");
+
+        expectedBooks.add(bookDto);
+
+        // Create Parameter for the query and sends a request
+        MultivaluedMap<String, String> params = new MultivaluedHashMap<>();
+        params.putSingle("title", "Unknown title");
+        Response response = getRequest(path, params).get();
+
+        Assert.assertEquals(200, response.getStatus());
+
+        // Collect the result from the response
+        ArrayList<BookDto> result = response.readEntity(BookList.class);
+
+        testArray(expectedBooks, result);
+    }
+
+    protected <T> void testArray(ArrayList<T> expected, ArrayList<T> actual) {
         Assertions.assertAll("Books", () -> {
             Assertions.assertNotNull(actual);
             Assertions.assertEquals(expected.size(), actual.size());
-            System.out.println(expected.get(0).getId());
-            System.out.println(actual.get(0).getId());
-            Assert.assertThat(actual, containsInAnyOrder(expected.toArray()));
+            assertThat(actual, containsInAnyOrder(expected.toArray()));
         });
     }
 
