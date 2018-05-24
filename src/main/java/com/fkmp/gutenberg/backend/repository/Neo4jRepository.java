de1d640057a57f6fe684802fada8564c9e2bc9dc
Mikkel Ziemer <mzhjensen@gmail.com>
2018-05-24 09:20:05 +0200

    Fixed some entity mapping

--- src/main/java/com/fkmp/gutenberg/backend/repository/Neo4jRepository.java
+++ src/main/java/com/fkmp/gutenberg/backend/repository/Neo4jRepository.java
@@ -1,7 +1,6 @@
 package com.fkmp.gutenberg.backend.repository;
 
 import com.fkmp.gutenberg.backend.model.neo4j.Book;
-import org.springframework.data.geo.Point;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -19,6 +18,10 @@ public interface Neo4jRepository extends CrudRepository<Book, String> {
     @Query("MATCH (n:Book) RETURN n LIMIT 10")
     List<Book> getBooksByAuthor(String authorName);
 
-    @Query("MATCH (n:Book) RETURN n LIMIT 10")
-    List<Book> getBooksByLocation(Double latitude, Double longitude);
+    @Query("WITH {lat} AS lat, {lon} AS lon\n" +
+            "MATCH (l:City) \n" +
+            "WHERE 2 * 6371 * asin(sqrt(haversin(radians(lat - toFloat(split(l.location, \",\")[0])))+ cos(radians(lat))* cos(radians(toFloat(split(l.location, \",\")[0])))* haversin(radians(lon - toFloat(split(l.location, \",\")[1]))))) < 20\n" +
+            "MATCH (l)-[:CONTAINS]-(book:Book)\n" +
+            "RETURN l")
+    List<Book> getBooksByLocation(@Param("lat") Double latitude, @Param("lon") Double longitude);
 }
