75e438373ac3f103fc92dadad75ffca960a168ba
Florent Haxha <kryptag@gmail.com>
2018-05-24 10:44:19 +0200

    last 2 queries done, now we need to do benchmarking and performance

--- src/main/java/com/fkmp/gutenberg/backend/repository/PostgreSQLRepository.java
+++ src/main/java/com/fkmp/gutenberg/backend/repository/PostgreSQLRepository.java
@@ -14,10 +14,10 @@ public interface PostgreSQLRepository extends CrudRepository<Book, String> {
     @Query(value = "SELECT DISTINCT ON (books.book_id) books.book_id, title FROM books join books_cities ON (books.book_id = books_cities.book_id) JOIN cities ON (books_cities.latitude = cities.latitude AND books_cities.longitude = cities.longitude) WHERE cities.name = ?1;", nativeQuery = true)
     List<Book> getBooksMentioningCity(String cityName);
 
-    @Query(value = "SELECT * FROM books LIMIT 10;", nativeQuery = true)
+    @Query(value = "SELECT name FROM cities JOIN books_cities ON (cities.latitude = books_cities.latitude AND cities.longitude = books_cities.longitude) JOIN books ON (books.book_id = books_cities.book_id) WHERE books.title = ?1;", nativeQuery = true)
     List<Book> getCitiesByTitle(String bookTitle);
 
-    @Query(value = "SELECT * FROM books LIMIT 10;", nativeQuery = true)
+    @Query(value = "SELECT * FROM books NATURAL JOIN authors_books NATURAL JOIN books_cities NATURAL JOIN cities WHERE author_id IN (SELECT author_id FROM authors WHERE name = ?1);", nativeQuery = true)
     List<Book> getBooksByAuthorName(String authorName);
 
     @Query(value = "SELECT title, books.book_id cities.name FROM books JOIN books_cities ON (books.book_id = books_cities.book_id) JOIN cities ON (books_cities.latitude = cities.latitude AND books_cities.longitude = cities.longitude) WHERE cities.location <@ circle(point'(?1)', 0.16);", nativeQuery = true)
